aac132209687a03065020d88de5df596a9537481
jesh-exe <jrmurodiya@gmail.com>
2025-06-13 21:50:51 +0530

    Ready for Virtual Hosting

--- JobVistaAPI/src/main/java/com/jobvista/repository/BioMarkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobvista.repository;
-
-import com.jobvista.entities.BioMarker;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BioMarkerRepository extends JpaRepository<BioMarker, Integer> {
-}
